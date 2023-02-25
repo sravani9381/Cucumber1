80f12245ac0d919bdfbedf01f6c44772d2ac41fe
sravani9381 <sravani.madasu@capgemini.com>
2023-02-25 13:41:40 +0530

    test

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,8 @@ import org.testng.annotations.DataProvider;
 // Cucumber options for running the test
 @CucumberOptions(features = "src/test/resources/features",
         glue = "steps",publish = true,
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"
+        ,"json:target/Cucumber-reports.json"},
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
